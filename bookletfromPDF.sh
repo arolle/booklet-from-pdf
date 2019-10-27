1b98daa71e82f03a61f45c17056fc7503c4a2f1b
arolle <1167984+arolle@users.noreply.github.com>
2019-10-27 09:30:55 +0100

    implement shellcheck suggestions

--- bookletfromPDF.sh
+++ bookletfromPDF.sh
@@ -6,25 +6,25 @@ set -euo pipefail
 # directory of program
 PROG_DIR=$(cd "$(dirname "$(readlink "${0}")")"; pwd)
 
-QPDF=$(which qpdf)
-[ -x "${QPDF}" ] && echo $QPDF
+QPDF="$(command -v qpdf)"
+[ -x "${QPDF}" ] && echo "$QPDF"
 [ -x "${QPDF}" ] || (echo "qpdf was not found in the path" && exit 1)
 [ -r "${1}" ] || (echo "given file '${1}' can not be read" && exit 1)
 OUTPUT="${1%.*}.booklet.pdf"
 
-PAGES=$("${QPDF}" --show-npages "${1}" | tr -d '\n ')
-m=$(($PAGES % 4))
+PAGES="$("${QPDF}" --show-npages "${1}" | tr -d '\n ')"
+m="$((PAGES % 4))"
 [ ! "$m" -eq 0 ] && (echo "number of pages '$PAGES' has to be a multiple of 4"; exit 1)
 
-PAGEORDER=$("${PROG_DIR}/page-order.sh" "${PAGES}" $2)
+PAGEORDER="$("${PROG_DIR}/page-order.sh" "${PAGES}" "$2")"
 echo "pages '$PAGES'"
 
-if [ $2-- == --rotate-back-page-- ]; then
-    echo Rotating back pages...
-    ROTATE=--rotate=+180:3,4$(c=5; while [ $c -lt $PAGES ]; do echo -n ,$(($c+2)),$(($c+3)); c=$(($c+4)); done)
+if [ "$2--" == --rotate-back-page-- ]; then
+    echo "Rotating back pages..."
+    ROTATE=--rotate=+180:3,4$(c=5; while [ $c -lt $PAGES ]; do echo -n   ,$(($c+2)),$(($c+3)); c=$(($c+4)); done)
 else
     ROTATE=''
 fi
-"${QPDF}" $ROTATE --empty --pages "${1}" $PAGEORDER -- "${OUTPUT}"
+"${QPDF}" "$ROTATE" --empty --pages "${1}" "$PAGEORDER" -- "${OUTPUT}"
 echo "written to '$OUTPUT'"
 
--- page-order.sh
+++ page-order.sh
@@ -7,21 +7,22 @@
 # check for non-empty positive input that is divisible by 4
 n="$1"
 [ -z "$n" ] && exit 1
-m=$(($n % 4))
+m="$((n % 4))"
 [ "$n" -lt 1 ] && exit 1
 [ ! "$m" -eq 0 ] && exit 1
 
 # limit for the sequence index
-seqlim=$((($n - 4) / 2))
+seqlim="$(((n - 4) / 2))"
 
 for x in $(seq 0 2 "$seqlim"); do
-  m1=$(($n - $x))
-  m2=$(($x + 1))
-  m3=$(($m2 + 1)) # $x + 2
-  m4=$(($m1 - 1)) # $n - $x - 1
-  if [ --$2-- == --rotate-back-page-- ]; then
+  m1="$((n - x))"
+  m2="$((x + 1))"
+  m3="$((m2 + 1))" # $x + 2
+  m4="$((m1 - 1))" # $n - $x - 1
+  if [ "--$2--" == --rotate-back-page-- ]; then
     echo "$m1,$m2,$m4,$m3"
   else
     echo "$m1,$m2,$m3,$m4"
   fi
 done | paste -sd "," -
+
